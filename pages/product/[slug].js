82773974d5274b07522d22ec575202e6b64cbdb7
Gagan Maheshwari, Vawsum <ggm@vawsum.com>
2022-10-24 13:59:19 +0530

    Update [slug].js
    Logic implementation remains

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,14 +8,93 @@ const ProductDetails = ({ productDetails, similarProducts }) =>
 {
 	const { image, name, details, price } = productDetails;
 
+	const [index, setIndex] = useState(0);
+
 	return (
 		<div>
 			<div className="product-detail-container">
 				<div>
 					<div className="image-container">
 						<img
-							src={urlFor(image && image[0])}
+							className="product-detail-image"
+							src={urlFor(image && image[index])}
 						/>
+					</div>
+
+					<div className="small-images-container">
+						{image?.map((item, i) => (
+							<img
+								src={urlFor(item)}
+								className={i === index ? "small-image selected-image" : "small-image"}
+								onMouseEnter={() => setIndex(i)}
+							/>
+						))}
+					</div>
+				</div>
+
+				<div className="product-details-desc">
+					<h1>
+						{name}
+					</h1>
+
+					<div className="reviews">
+						<div>
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiOutlineStar />
+						</div>
+						<p>
+							(20)
+						</p>
+					</div>
+
+					<h4>Details: </h4>
+					<p>{details}</p>
+					<p className="price">₹ {price}</p>
+
+					<div className="quantity">
+						<h3>
+							Quantity:
+						</h3>
+
+						<p className="quantity-desc">
+							<span className="minus" onClick="">
+								<AiOutlineMinus />
+							</span>
+
+							<span className="num">
+								0
+							</span>
+
+							<span className="plus" onClick="">
+								<AiOutlinePlus />
+							</span>
+						</p>
+					</div>
+
+					<div className="buttons">
+						<button type="button" className="add-to-cart" onClick="">
+							Add to Cart
+						</button>
+						<button type="button" className="buy-now" onClick="">
+							Buy Now
+						</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="maylike-products-wrapper">
+				<h2>
+					You may also like
+				</h2>
+
+				<div className="marquee">
+					<div className="maylike-products-container track">
+						{similarProducts.map((item) => (
+							<Product key={item._id} product={item} />
+						))}
 					</div>
 				</div>
 			</div>
@@ -25,7 +104,7 @@ const ProductDetails = ({ productDetails, similarProducts }) =>
 
 export const getStaticPaths = async () =>
 {
-	const productsQuery = `*[_type] == "product"]
+	const query = `*[_type == "product"]
 	{
 		slug
 		{
@@ -33,7 +112,8 @@ export const getStaticPaths = async () =>
 		}
 	}`;
 
-	const products = await client.fetch(productsQuery);
+	const products = await client.fetch(query);
+
 	const paths = products.map((product) => (
 		{
 			params:
